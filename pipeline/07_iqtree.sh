88f3b5b1a5ea8118771ab03ffaa0f4d9c0fb5b46
Lotus Lofgren <LotusLofgren@gmail.com>
2021-01-01 14:40:08 -0800

    Create 07_iqtree.sh

---
+++ pipeline/07_iqtree.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#SBATCH -p intel -N 1 -n 6 --mem 16gb  --out logs/IQTREE2.log
+
+module load IQ-TREE/2.1.1
+
+source config.txt
+# this will fail but will generate file with fixed sites removed
+cd strain_tree
+iqtree2 -s $PREFIX.SNP.mfa -m GTR+ASC -nt AUTO -B 1000
